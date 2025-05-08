7a77cdc182faee48357b8f556a471f130afe8393
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-05-07 22:16:48 -0700

    Introduce Joke struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Joke {
+    id: Option<u32>,
+    joke: Option<String>,
+    setup: Option<String>,
+    delivery: Option<String>,
+    category: Option<String>,
+    error: bool,
+}
+
 fn main() {
     println!("Hello, world!");
 }
