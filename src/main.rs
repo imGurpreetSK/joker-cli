6b4e29b6e5a3644a3426611a9cd4d76588c22098
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-05-07 22:17:21 -0700

    Fetch and print joke to console.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,27 @@ struct Joke {
     error: bool,
 }
 
-fn main() {
-    println!("Hello, world!");
+async fn get_joke() -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get("https://v2.jokeapi.dev/joke/Any").await?;
+    let joke = response.json::<Joke>().await?;
+
+    match joke.error {
+        true => {
+            println!("Error occurred while fetching joke");
+        }
+        false => {
+            match (joke.joke, joke.setup, joke.delivery) {
+                (Some(joke), _, _) => println!("{}", joke),
+                (_, Some(setup), Some(delivery)) => println!("{}\n\n{}", setup, delivery),
+                _ => println!("No joke found"),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    get_joke().await
 }
